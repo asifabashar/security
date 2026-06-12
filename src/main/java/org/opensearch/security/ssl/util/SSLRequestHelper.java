e783b067376b9f85ac545459b627d1c2ce7525cd
Asif Bashar <asif.bashar@gmail.com>
2026-06-11 23:10:30 -0700

    updated header cert check
    Signed-off-by: Asif Bashar <asif.bashar@gmail.com>

--- src/main/java/org/opensearch/security/ssl/util/SSLRequestHelper.java
+++ src/main/java/org/opensearch/security/ssl/util/SSLRequestHelper.java
@@ -21,6 +21,8 @@ import java.io.ByteArrayInputStream;
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.IOException;
+import java.net.URLDecoder;
+import java.nio.charset.StandardCharsets;
 import java.nio.file.Path;
 import java.security.GeneralSecurityException;
 import java.security.KeyStore;
@@ -31,13 +33,17 @@ import java.security.cert.CertificateException;
 import java.security.cert.CertificateFactory;
 import java.security.cert.TrustAnchor;
 import java.security.cert.X509Certificate;
-import java.util.*;
+import java.util.Arrays;
+import java.util.Base64;
+import java.util.Collection;
+import java.util.Date;
+import java.util.Enumeration;
+import java.util.HashSet;
+import java.util.Set;
 import java.util.stream.Collectors;
 import javax.net.ssl.SSLEngine;
 import javax.net.ssl.SSLPeerUnverifiedException;
 import javax.net.ssl.SSLSession;
-import java.net.URLDecoder;
-import java.nio.charset.StandardCharsets;
 
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
@@ -82,7 +88,7 @@ public class SSLRequestHelper {
             this.protocol = protocol;
             this.cipher = cipher;
             this.localCertificates = localCertificates;
-            this.headerCerts =headerCerts;
+            this.headerCerts = headerCerts;
             this.headerPrincipal = headerPrincipal;
         }
 
@@ -140,33 +146,27 @@ public class SSLRequestHelper {
         final String cipher = session.getCipherSuite();
         String principal = null;
         String headerCertPrincipal = null;
-        final String clientCertHeaderName = settings.get("clientCertHeaderName","ClientCert");
+        final String clientCertHeaderName = settings.get("clientCertHeaderName", "ClientCert");
         final boolean isURLEncoded = Boolean.parseBoolean(settings.get("isURLEncoded", "true"));
 
-
         String clientCertEncoded = request.header(clientCertHeaderName);
-        if (isURLEncoded){
-            String clientCertFromHeader = URLDecoder.decode(
-                    clientCertEncoded,
-                    StandardCharsets.UTF_8
-            );
+        if (isURLEncoded) {
+            String clientCertFromHeader = URLDecoder.decode(clientCertEncoded, StandardCharsets.UTF_8);
             byte[] decodedClientCert = Base64.getDecoder().decode(clientCertFromHeader);
 
             CertificateFactory factory = null;
             try {
                 factory = CertificateFactory.getInstance("X.509");
                 x509HeaderCerts = new X509Certificate[1];
-                x509HeaderCerts[0] = (X509Certificate)
-                        factory.generateCertificate(new ByteArrayInputStream( decodedClientCert));
-                headerCertPrincipal = principalExtractor == null ? null : principalExtractor.extractPrincipal(x509HeaderCerts[0], Type.HTTP);
+                x509HeaderCerts[0] = (X509Certificate) factory.generateCertificate(new ByteArrayInputStream(decodedClientCert));
+                headerCertPrincipal = principalExtractor == null
+                    ? null
+                    : principalExtractor.extractPrincipal(x509HeaderCerts[0], Type.HTTP);
             } catch (CertificateException e) {
 
             }
 
         }
-
-
-
 
         if (engine.getNeedClientAuth() || engine.getWantClientAuth()) {
             Certificate[] certs = null;
@@ -190,7 +190,7 @@ public class SSLRequestHelper {
             localCerts = Arrays.stream(session.getLocalCertificates()).map(X509Certificate.class::cast).toArray(X509Certificate[]::new);
         }
 
-        return new SSLInfo(x509Certs, principal, protocol, cipher, localCerts,x509HeaderCerts,headerCertPrincipal);
+        return new SSLInfo(x509Certs, principal, protocol, cipher, localCerts, x509HeaderCerts, headerCertPrincipal);
     }
 
     private static void validatePeerCerts(final X509Certificate[] x509Certs, final Settings settings, final Path configPath)
